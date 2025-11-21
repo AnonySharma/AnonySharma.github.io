cb8f218188398274494317a517c4f71fe6186d5e
Ankit Kumar Sharma <ankit.kumar6@ankitkum-ltmj2hk.internal.salesforce.com>
2025-11-21 17:31:03 +0530

    Removed old files

--- res/script.js
+++
@@ -1,9 +0,0 @@
-temp=0;
-
-function onClicked() {
-	temp++;
-	if(temp%2==0)
-		document.getElementById("textt").style.color = "#FFFF32";
-	else
-		document.getElementById("textt").style.color = "#000";
-}
